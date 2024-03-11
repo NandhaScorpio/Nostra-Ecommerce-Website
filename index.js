e4974374cde16522c6b3d1d3037307f20ef4a407
NandhaScorpio <gokulamstore2010@gmail.com>
2024-03-11 16:20:37 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,12 @@
+var xmark = document.getElementById("xmark")
+var deleteTag = document.querySelector(".delete-tag")
+
+xmark.addEventListener("click", function () {
+    deleteTag.style.top = "60%"
+    deleteTag.style.display = "none"
+})
+
+var i1 = document.getElementById("i1")
+var i2 = document.getElementById("i2")
+var i3 = document.getElementById("i3")
+
